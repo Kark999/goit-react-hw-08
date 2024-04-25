71492443ad518f29d5ab229dc4f9b9f697a3a45d
Andrey Svyrskyi <akkad1v@gmail.com>
2024-04-25 03:51:58 +0300

    'update'

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,5 +1,45 @@
+import { NavLink } from "react-router-dom";
+import clsx from "clsx";
+import { Routes, Route } from "react-router-dom";
+import Loader from "../../Loader/Loader";
+import { Suspense, lazy } from "react";
+import css from "./Navigation.module.css";
+
+const HomePage = lazy(() => import("../../pages/HomePage"));
+const RegistrationPage = lazy(() => import("../../pages/RegistrationPage"));
+const LoginPage = lazy(() => import("../../pages/LoginPage"));
+const ContactsPage = lazy(() => import("../../pages/ContactsPage"));
+const NotFoundPage = lazy(() => import("../../pages/NotFoundPage"));
+
+const getNavLinkClassName = ({ isActive }) =>
+  clsx(css.navLink, { [css.active]: isActive });
+
 const Navigation = () => {
-  return <div>Navigation</div>;
+  return (
+    <div>
+      <header>
+        <nav className={css.nav}>
+          <NavLink className={getNavLinkClassName} to="/">
+            Home
+          </NavLink>
+          <NavLink className={getNavLinkClassName} to="/movies">
+            Movies
+          </NavLink>
+        </nav>
+      </header>
+      <main>
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/register" element={<RegistrationPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/contacts" element={<ContactsPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </Suspense>
+      </main>
+    </div>
+  );
 };
 
 export default Navigation;
