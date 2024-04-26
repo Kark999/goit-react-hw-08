669a1be9fbfa203950e83c5481d7512888d39e97
Andrey Svyrskyi <akkad1v@gmail.com>
2024-04-26 06:30:51 +0300

    'update'

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,6 +1,17 @@
-// import css from './AppBar.module.css'
-const AppBar = () => {
-  return <div>AppBar</div>;
-};
+import { Navigation } from "../Navigation/Navigation";
+import { UserMenu } from "../UserMenu/UserMenu";
+import { useSelector } from "react-redux";
+import { AuthNav } from "../AuthNav/AuthNav";
+import { selectIsLoggedIn } from "../../redux/auth/selectors";
+import css from "./AppBar.module.css";
 
-export default AppBar;
+export const AppBar = () => {
+  const { isLoggedIn } = useSelector(selectIsLoggedIn);
+
+  return (
+    <header className={css.header}>
+      <Navigation />
+      {isLoggedIn ? <UserMenu /> : <AuthNav />}
+    </header>
+  );
+};
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,51 +1,73 @@
 import { NavLink } from "react-router-dom";
-import clsx from "clsx";
-import { Routes, Route } from "react-router-dom";
-import Loader from "../Loader/Loader";
-import { Suspense, lazy } from "react";
+import { useSelector } from "react-redux";
+import { selectIsLoggedIn } from "../../redux/auth/selectors";
 import css from "./Navigation.module.css";
 
-const HomePage = lazy(() => import("../../pages/HomePage"));
-const RegistrationPage = lazy(() => import("../../pages/RegistrationPage"));
-const LoginPage = lazy(() => import("../../pages/LoginPage"));
-const ContactsPage = lazy(() => import("../../pages/ContactsPage"));
-const NotFoundPage = lazy(() => import("../../pages/NotFoundPage"));
+export const Navigation = () => {
+  const { isLoggedIn } = useSelector(selectIsLoggedIn);
 
-const getNavLinkClassName = ({ isActive }) =>
-  clsx(css.navLink, { [css.active]: isActive });
-
-const Navigation = () => {
   return (
-    <div>
-      <header className={css.header}>
-        <nav className={css.nav}>
-          <NavLink className={getNavLinkClassName} to="/">
-            Home
-          </NavLink>
-          <NavLink className={getNavLinkClassName} to="/register">
-            Register
-          </NavLink>
-          <NavLink className={getNavLinkClassName} to="/login">
-            Log In
-          </NavLink>
-          <NavLink className={getNavLinkClassName} to="/contacts">
-            Contacts
-          </NavLink>
-        </nav>
-      </header>
-      <main>
-        <Suspense fallback={<Loader />}>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/register" element={<RegistrationPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/contacts" element={<ContactsPage />} />
-            <Route path="*" element={<NotFoundPage />} />
-          </Routes>
-        </Suspense>
-      </main>
-    </div>
+    <nav>
+      <NavLink className={css.link} to="/">
+        Home
+      </NavLink>
+      {isLoggedIn && (
+        <NavLink className={css.link} to="/contacts">
+          Contacts
+        </NavLink>
+      )}
+    </nav>
   );
 };
 
-export default Navigation;
+// import { NavLink } from "react-router-dom";
+// import clsx from "clsx";
+// import { Routes, Route } from "react-router-dom";
+// import Loader from "../Loader/Loader";
+// import { Suspense, lazy } from "react";
+// import css from "./Navigation.module.css";
+
+// const HomePage = lazy(() => import("../../pages/HomePage"));
+// const RegistrationPage = lazy(() => import("../../pages/RegistrationPage"));
+// const LoginPage = lazy(() => import("../../pages/LoginPage"));
+// const ContactsPage = lazy(() => import("../../pages/ContactsPage"));
+// const NotFoundPage = lazy(() => import("../../pages/NotFoundPage"));
+
+// const getNavLinkClassName = ({ isActive }) =>
+//   clsx(css.navLink, { [css.active]: isActive });
+
+// const Navigation = () => {
+//   return (
+//     <div>
+//       <header className={css.header}>
+//         <nav className={css.nav}>
+//           <NavLink className={getNavLinkClassName} to="/">
+//             Home
+//           </NavLink>
+//           <NavLink className={getNavLinkClassName} to="/register">
+//             Register
+//           </NavLink>
+//           <NavLink className={getNavLinkClassName} to="/login">
+//             Log In
+//           </NavLink>
+//           <NavLink className={getNavLinkClassName} to="/contacts">
+//             Contacts
+//           </NavLink>
+//         </nav>
+//       </header>
+//       <main>
+//         <Suspense fallback={<Loader />}>
+//           <Routes>
+//             <Route path="/" element={<HomePage />} />
+//             <Route path="/register" element={<RegistrationPage />} />
+//             <Route path="/login" element={<LoginPage />} />
+//             <Route path="/contacts" element={<ContactsPage />} />
+//             <Route path="*" element={<NotFoundPage />} />
+//           </Routes>
+//         </Suspense>
+//       </main>
+//     </div>
+//   );
+// };
+
+// export default Navigation;
