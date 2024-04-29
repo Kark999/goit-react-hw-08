2ecc6c3e554911dabeac2be0def44907d002e4a8
Andrey Svyrskyi <akkad1v@gmail.com>
2024-04-29 23:46:37 +0300

    'update'

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { Provider } from "react-redux";
 import { persistor, store } from "./redux/store.js";
 
 import "./index.css";
-import { HelmetProvider } from "react-helmet";
+import { HelmetProvider } from "react-helmet-async";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
