4b213e0d5b8cdb3cdf03b2b7a0afcd1cc46c3ac4
Andrey Svyrskyi <akkad1v@gmail.com>
2024-04-30 03:44:25 +0300

    'update'

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Layout from "./components/Layout/Layout";
 import "./App.css";
 import { refreshUser } from "./redux/auth/operations";
 import { useDispatch } from "react-redux";
+import RestrictedRoute from "./components/RestrictedRoute/RestrictedRoute";
 
 const HomePage = lazy(() => import("./pages/HomePage"));
 const RegistrationPage = lazy(() => import("./pages/RegistrationPage"));
@@ -25,8 +26,22 @@ function App() {
         <Suspense fallback={<Loader />}>
           <Routes>
             <Route path="/" element={<HomePage />} />
-            <Route path="/register" element={<RegistrationPage />} />
-            <Route path="/login" element={<LoginPage />} />
+            <Route
+              path="/register"
+              element={
+                <RestrictedRoute>
+                  <RegistrationPage />
+                </RestrictedRoute>
+              }
+            />
+            <Route
+              path="/login"
+              element={
+                <RestrictedRoute>
+                  <LoginPage />
+                </RestrictedRoute>
+              }
+            />
             <Route path="/contacts" element={<ContactsPage />} />
             <Route path="*" element={<NotFoundPage />} />
           </Routes>
